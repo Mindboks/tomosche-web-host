651574c53dce607e1ae22b0e0e39dfd40d4171c0
Mindboks <mindboks@gmail.com>
2026-08-12 14:55:10 +0900

    v1.0.1.2: フィードバック送信のデバッグログを強化

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ================================================================
-// Tomosche 設定 v1.0.1.1
+// Tomosche 設定 v1.0.1.2
 // ================================================================
-const APP_VERSION = '1.0.1.1';
+const APP_VERSION = '1.0.1.2';
 const APP_NAME = 'Tomosche';
 const APP_URL = 'https://tomosche.com';
 
