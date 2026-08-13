bdad64fc944e9b2dab230372ceae6c8213800e01
Mindboks <mindboks@gmail.com>
2026-08-13 10:21:23 +0900

    v1.0.3.1: QRコードにfriendIdを含め、友達追加後にHomeが更新されるよう修正

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ================================================================
 // Tomosche 設定 
 // ================================================================
-const APP_VERSION = '1.0.3.0';
+const APP_VERSION = '1.0.3.1';
 const APP_NAME = 'Tomosche';
 const APP_URL = 'https://tomosche.com';
 
