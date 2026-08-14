a6dfc258c66db1b5d12d546ac7a241b861fb2bea
Mindboks <mindboks@gmail.com>
2026-08-14 20:30:48 +0900

    v1.1.1.5

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ================================================================
 // Tomosche 設定 
 // ================================================================
-const APP_VERSION = '1.1.1.4';
+const APP_VERSION = '1.1.1.5';
 const APP_NAME = 'Tomosche';
 const APP_URL = 'https://tomosche.com';
 
